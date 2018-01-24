99c8372f6cc7527f92c16c9b6e522bdcbf36aa4f
KSelivanov <kirill_selivanov_21@mail.ru>
2018-01-24 23:36:52 +0500

    fix

--- main.py
+++ main.py
@@ -29,14 +29,16 @@ class PointGenerator:
         else:
             raise RuntimeError(str.format('Unknown Crossover Method : {0}', self.crossover_method))
 
+
 class Solver_8_queens:
     board_count = 7
 
-    def __init__(self, pop_size=1000, cross_prob=0.30, mut_prob=0.25, crossover_method=CrossoverMethod.SINGLEPOINT):
+    def __init__(self, pop_size=1000, cross_prob=0.30, mut_prob=0.25, crossover_method=CrossoverMethod.SINGLEPOINT, selection_method = SelectionMethod.WHEEL):
         self.pop_size = pop_size
         self.cross_prob = cross_prob
         self.mut_prob = mut_prob
         self.crossover_method = crossover_method
+        self.selection_method = selection_method
 
     def solve(self, min_fitness=1, max_epochs=1000):
         best_fit = 0
@@ -65,9 +67,14 @@ class Solver_8_queens:
                         best_individual = individual
             if is_done:
                 break
+            if self.selection_method == SelectionMethod.WHEEL:
+                population = self.wheel_selection(population, fitnesses, self.pop_size)
+            elif self.selection_method == SelectionMethod.TOURNAMENT:
+                population = self.tournament_selection(population, fitnesses, self.pop_size)
+            else:
+                RuntimeError(str.format('Unknown Selection Method : {0}', self.selection_method))
 
-            population = self.selection(population, fitnesses, self.pop_size)
-            population = self.cross_population(population, self.cross_prob)
+            population = self.crossover_population(population, self.cross_prob)
             population = self.mutate(population, self.mut_prob)
             epoch_num += 1
         return best_fit, epoch_num, self.visualization(best_individual)
@@ -77,7 +84,8 @@ class Solver_8_queens:
             random.randint(0, Solver_8_queens.board_count) for _ in range(0, Solver_8_queens.board_count + 1)
         ] for _ in range(0, self.pop_size)]
 
-    def visualization(self, individual):
+    @staticmethod
+    def visualization(individual):
         """
         Function for visualizing answer
         :param individual: vector when index - x in chess board and item - y in chess board
@@ -102,7 +110,8 @@ class Solver_8_queens:
 
         return "".join(result)
 
-    def cross(self, first_chromosome, second_chromosome, point_gen):
+    @staticmethod
+    def crossover(first_chromosome, second_chromosome, point_gen):
 
         first_mask = Solver_8_queens.board_count >> point_gen.get_point()
         second_mask = first_mask ^ Solver_8_queens.board_count
@@ -112,23 +121,24 @@ class Solver_8_queens:
 
         return first_child_chromosome, second_child_chromosome
 
-    def cross_population(self, population, prop):
+    def crossover_population(self, population, prop):
         result = []
-        for first_individ, second_individ in self.parents_choise(population):
+        for first_individ, second_individ in self.parents_choiсe(population):
             if prop >= random.uniform(0, 1):
                 first_child = []
                 second_child = []
                 point_gen = PointGenerator(self.crossover_method,
-                                           Solver_8_queens.board_count.bit_length() - 1)  # random.randint(0, Solver_8_queens.board_count.bit_length() - 1)
+                                           Solver_8_queens.board_count.bit_length() - 1)
                 for index in range(0, len(first_individ)):
-                    first_child_chromosome, second_child_chromosome = self.cross(first_individ[index],
-                                                                                 second_individ[index], point_gen)
+                    first_child_chromosome, second_child_chromosome = self.crossover(first_individ[index],
+                                                                                     second_individ[index], point_gen)
                     first_child.append(first_child_chromosome)
                     second_child.append(second_child_chromosome)
                 result.extend((first_child, second_child))
         return result
 
-    def mutate(self, population, prop):
+    @staticmethod
+    def mutate(population, prop):
         for individ in population:
             if prop >= random.uniform(0, 1):
                 index_gen_for_mutate = random.randint(0, len(individ) - 1)
@@ -136,24 +146,30 @@ class Solver_8_queens:
 
         return population
 
-    def parents_choise(self, population):
+    def parents_choiсe(self, population):
         for _ in range(0, len(population)):
-            yield population[random.randint(0, len(population) - 1)], population[random.randint(0, len(population) - 1)]
+            yield self._get_random_population_item(population), self._get_random_population_item(population)
 
-    def fitness_function(self, individ):
+    @staticmethod
+    def fitness_function(individual):
         combination_weight = 1 / len(list(itertools.combinations(range(0, Solver_8_queens.board_count + 1), 2)))
         result = 0
-        coordinates_individ = list(enumerate(individ))
+        coordinates_individual = list(enumerate(individual))
 
-        for current_x, current_y in coordinates_individ:
-            for checked_x, checked_y in coordinates_individ[current_x + 1:]:
+        for current_x, current_y in coordinates_individual:
+            for checked_x, checked_y in coordinates_individual[current_x + 1:]:
                 x_diff = checked_x - current_x
                 y_diff = checked_y - current_y
                 if y_diff == 0 or (current_y + x_diff == checked_y) or (current_y - x_diff == checked_y):
                     result += 1
         return 1 - result * combination_weight
 
-    def selection(self, population, fitnesses, num):
+    @staticmethod
+    def _get_random_population_item(population):
+        return population[random.randint(0, len(population) - 1)]
+
+    @staticmethod
+    def wheel_selection(population, fitnesses, num):
         total_weight = sum(fitnesses)
         fitnesses_refs = [f / total_weight for f in fitnesses]
 
@@ -170,9 +186,21 @@ class Solver_8_queens:
                     break
         return result
 
+    @staticmethod
+    def tournament_selection(population, fitnesses, num):
+        result = []
+        for _ in range(0, num):
+            first_contender_index = random.randint(0, len(population) - 1)
+            second_contender_index = random.randint(0, len(population) - 1)
+
+            if fitnesses[first_contender_index] > fitnesses[second_contender_index]:
+                result.append(population[first_contender_index])
+            else:
+                result.append(population[second_contender_index])
+        return result
+
 
 if __name__ == "__main__":
-    solver = Solver_8_queens()
+    solver = Solver_8_queens(selection_method=SelectionMethod.TOURNAMENT)
     best_fit, epoch_num, visual = solver.solve(max_epochs=None)
     print(str.format("best_fit : {0}\nepoch_num: {1}\nvisual:\n{2}", best_fit, epoch_num, visual))
-    # print(solver.generate_first_population())
